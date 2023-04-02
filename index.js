f8fad0e1af2dc716ef2a5275e223317c2d01aa86
Bharat155 <52926295+Bharat155@users.noreply.github.com>
2023-04-02 18:31:41 +0530

    cors solution 2

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const agencyRouter = require('./routes/agencyAuth');
 const jobRouter = require('./routes/jobs');
 const cors = require('cors');
 
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -14,14 +15,15 @@ app.use(clientRouter);
 app.use(freelancerRouter);
 app.use(agencyRouter);
 app.use(jobRouter);
-app.use(cors());
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
+//cors
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
 
 const DB = "mongodb+srv://bharatahuja:skillvalley@cluster0.atz7e6q.mongodb.net/?retryWrites=true&w=majority";
 
